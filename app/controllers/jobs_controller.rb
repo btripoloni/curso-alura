760131af964ca15a8ab1f0c117d042beeb5db132
bruno_tripoloni <bruno.tripoloni@gmail.com>
2014-03-12 23:54:15 -0300

    First Commit.

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,97 @@
+class JobsController < ApplicationController
+  before_filter :authorize_company, only: [:new, :create, :edit, :update, :destroy]
+  # GET /jobs
+  # GET /jobs.json
+  def index
+    @jobs = Job.most_recent.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @jobs }
+    end
+  end
+
+  def premium
+    @jobs = Job.where(premium: true).most_recent.
+      paginate(page: params[:page], per_page: 10)
+  end
+
+  # GET /jobs/1
+  # GET /jobs/1.json
+  def show
+    @job = Job.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @job }
+    end
+  end
+
+  # GET /jobs/new
+  # GET /jobs/new.json
+  def new
+    @job = current_company.jobs.build
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @job }
+    end
+  end
+
+  # GET /jobs/1/edit
+  def edit
+    @job = current_company.jobs.find(params[:id])
+  end
+
+  # POST /jobs
+  # POST /jobs.json
+  def create
+    @job = current_company.jobs.build(params[:job])
+
+    respond_to do |format|
+      if @job.save
+        format.html { redirect_to @job, notice: 'Job was successfully created.' }
+        format.json { render json: @job, status: :created, location: @job }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @job.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /jobs/1
+  # PUT /jobs/1.json
+  def update
+    @job = current_company.jobs.find(params[:id])
+
+    respond_to do |format|
+      if @job.update_attributes(params[:job])
+        format.html { redirect_to @job, notice: 'Job was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @job.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /jobs/1
+  # DELETE /jobs/1.json
+  def destroy
+    @job = current_company.jobs.find(params[:id])
+    @job.destroy
+
+    respond_to do |format|
+      format.html { redirect_to jobs_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def authorize_company
+    unless current_company
+      redirect_to root_path, alert: "You need stay logged to acess this page"
+    end
+  end
+end
